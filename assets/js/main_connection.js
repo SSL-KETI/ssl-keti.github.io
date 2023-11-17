b6f7f1c92bbce34b24493a0e6ddbf3bb907f5e3b
ssl-keti <ssl.keti1@gmail.com>
2023-11-17 16:58:16 +0900

    Attach 2023 dockers in part2

--- assets/js/main_connection.js
+++ assets/js/main_connection.js
@@ -80,6 +80,57 @@ function visualize_json(json_input, canvas_id) {
     sys.graft(data);
 }
 
+// 파일을 선택하지 않았을 때 원본 이미지 & 마스크 이미지를 인터넷에서 불러오는 함수
+function getBase64ImageFromURLBothImages(input_url, num, img_url, mask_url, checked_boxes) {
+    var img = new Image();
+    var mask = new Image();
+    img.setAttribute('crossOrigin', 'anonymous');
+    mask.setAttribute('crossOrigin', 'anonymous');
+    
+    img.onload = imgData => {
+        mask.onload = imgData => {
+            var canvas = document.createElement("canvas");
+            canvas.width = img.width;
+            canvas.height = img.height;
+            var ctx = canvas.getContext("2d");
+            ctx.drawImage(img, 0, 0);
+            var dataURL_original = canvas.toDataURL("image/png");
+            var img_original = dataURL_original;
+            var commaIndex = img_original.indexOf(",");
+            img_original = img_original.slice(commaIndex+1);
+            
+            var canvas2 = document.createElement("canvas");
+            canvas2.width = mask.width;
+            canvas2.height = mask.height;
+            var ctx2 = canvas2.getContext("2d");
+            ctx2.drawImage(mask, 0, 0);
+            var dataURL_mask = canvas2.toDataURL("image/png");
+            var img_mask = dataURL_mask;
+            var commaIndex = img_mask.indexOf(",");
+            img_mask = img_mask.slice(commaIndex+1);
+
+            var scene_graph = document.getElementById("json_preview"+num).innerText;
+            scene_graph = JSON.parse(scene_graph);
+
+            var jsonArray = new Object();
+            jsonArray['image_contents'] = img_original;
+            jsonArray['mask_contents'] = img_mask;
+            jsonArray['scene_graph'] = scene_graph;
+            jsonArray = JSON.stringify(jsonArray);
+            if (num == 4) {
+                for(var i=0; i < checked_boxes.length; i++) {
+                    postData(input_url+checked_boxes[i], jsonArray, num);
+                }
+            }
+            else {
+                postData(input_url, jsonArray, num)
+            }
+        }
+    }
+    img.src = img_url;
+    mask.src = mask_url;
+}
+
 function postData(input_url, jsonArray, num) {
     $.ajax({
         type: "POST",
@@ -88,11 +139,44 @@ function postData(input_url, jsonArray, num) {
         success:function(data){
             var obj = JSON.parse(data);
             console.log(obj);
-            if (num == 3) {
-                $('#result').html("");
-                document.getElementById('json_results'+num).style.visibility = 'visible';
-                $('#json_results'+num).html(JSON.stringify(obj.scene_graph_json));
-                visualize_json(JSON.stringify(obj.scene_graph_json), '#json_canvas_result'+num);
+            if (num == 1) {
+                $('#json_results'+num).html("");
+                if(input_url.endsWith('POSTECH')) { // POSTECH
+                    var jsonSceneGraph = obj.scene_graph_json;
+                    document.getElementById('json_results1_postech').style.visibility = 'visible';
+                    $('#json_results1_postech').html(JSON.stringify(jsonSceneGraph));
+
+                    var resultSceneGraph = document.createElement("img");
+                    resultSceneGraph.setAttribute("src", 'data:image/png;base64,' + obj.scene_graph_img); 
+                    resultSceneGraph.setAttribute("style", "max-width: 70%; height: auto;"); 
+                    $('#img_results1_postech').html("");
+                    document.querySelector('#img_results1_postech').appendChild(resultSceneGraph);
+                    } 
+                else { // KETI
+                    var jsonSceneGraph = obj.scene_graph_json;
+                    document.getElementById('json_results1_keti').style.visibility = 'visible';
+                    $('#json_results1_keti').html(JSON.stringify(jsonSceneGraph));
+                    
+                    var resultSceneGraph = document.createElement("img");
+                    resultSceneGraph.setAttribute("src", 'data:image/png;base64,' + obj.scene_graph_img); 
+                    resultSceneGraph.setAttribute("style", "max-width: 70%; height: auto;"); 
+                    $('#img_results1_keti').html("");
+                    document.querySelector('#img_results1_keti').appendChild(resultSceneGraph);
+                    /*
+                    원래 KETI의 결과에는 object detection도 포함되어 있음.
+                    이 부분을 포함시키면 "파일 미리보기"의 이미지가 object detection되어 bounding box가 생긴 이미지로 바뀜.
+                    var resultImage = document.createElement("img");
+                    resultImage.setAttribute("src", 'data:image/png;base64,' + obj.image_contents); 
+                    resultImage.setAttribute("style", "max-width: 100%; height: auto;"); 
+                    $('#image_container'+num).empty();
+                    document.querySelector('#image_container'+num).appendChild(resultImage); 
+                    */
+                }	
+            }
+            else if (num == 2 || num == 3) {
+                var scene_graph_result = JSON.stringify(obj.scene_graph_json);
+                $('#json_results'+num).html(scene_graph_result);
+                visualize_json(scene_graph_result, '#json_canvas_result'+num);
             }
             else if (num == 4) {
                 $('#image_result4_snu').html("");
